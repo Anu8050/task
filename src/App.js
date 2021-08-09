305256e1d751b8f2e5730c66a83af204a6dc5c02
Anusha <anusgowda050@gmail.com>
2021-08-09 17:52:46 +0530

    install react-pdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from 'react-dom';
+const { createInvoice } = require("./create.js");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component 
+{
+  shoot() {
+    const invoice = { 
+      shipping: {
+        name: "Anu",
+        address: "Mirle",
+        city: "k.r.nagar",
+        state: "karnataka",
+        country: "india",
+        postal_code: 549603
+      },
+      items: [
+        {
+          item: "TC 100",
+          description: "Toner Cartridge",
+          quantity: 2,
+          amount: 6000
+        },
+        {
+          item: "USB_EXT",
+          description: "USB Cable Extender",
+          quantity: 1,
+          amount: 2000
+        },
+        {
+            item: "USB",
+            description: "Cable ",
+            quantity: 1,
+            amount: 5000
+          }
+      ],
+      
+      subtotal: 9000,
+      paid: 0,
+      invoice_nr: 1234
+    };
+    createInvoice(invoice, "invoice.pdf");
+  }
+  render() {
+    return (
+      <button onClick={this.shoot}>Click Me!</button>
+    );
+  }
 }
-
+ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
